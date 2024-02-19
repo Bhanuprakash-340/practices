2a3f798b683c1c22ee7d8aaa6478410a6710bbd9
Bhanuprakash-340 <bhanu150940@gmail.com>
2024-02-19 18:45:29 +0530

    bhanu2 changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HoverCounter from './components/HoverCounter';
 function App() {
   return (
     <div className="App">
-      <ClickCounter name={'Bhanu prakash react developer'}/>
+      <ClickCounter name={'Bhanu prakash react developer bhanu prakash'}/>
       <HoverCounter/>
     </div>
   );
