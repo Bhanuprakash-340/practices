d364f4e285d6ea816d32cd974aab1943383d69cc
Bhanuprakash-340 <bhanu150940@gmail.com>
2024-03-04 12:59:43 +0530

    changes in need

--- src/components/MUI/MuiTypography.jsx
+++ src/components/MUI/MuiTypography.jsx
@@ -6,6 +6,7 @@ const MuiTypography = () => {
     <div>
         <Typography variant="h1">h1 Heading</Typography>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere fuga blanditiis odit nisi dolorem aliquid enim nemo porro recusandae, deserunt cum esse quod tempore nostrum ut dolore ullam, dignissimos assumenda!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas vel molestias quam ipsum velit, sunt rem cum cumque enim, aspernatur atque ducimus corrupti maiores dignissimos ex qui nisi. Earum, soluta.</p>
         <Typography variant="h2">h2 Heading</Typography>
         <Typography variant="h3">h3 Heading</Typography>
         <Typography variant="h4">h4 Heading</Typography>
