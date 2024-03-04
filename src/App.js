c4d2ec44974eb9a54ac79df43bc5ecce7fe960da
Bhanuprakash-340 <bhanu150940@gmail.com>
2024-03-04 12:16:09 +0530

    radio-and-switch-mui

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
-// import ClickCounter from './components/ClickCounter';
-// import MuiButton from './components/MUI/MuiButton';
-// import HoverCounter from './components/HoverCounter';
-// import MuiTypography from './components/MUI/MuiTypography';
-// import MuiTextFields from './components/MUI/MuiTextFields';
+import ClickCounter from './components/ClickCounter';
+import MuiButton from './components/MUI/MuiButton';
+import HoverCounter from './components/HoverCounter';
+import MuiTypography from './components/MUI/MuiTypography';
+import MuiTextFields from './components/MUI/MuiTextFields';
 import MuiSelect from './components/MUI/MuiSelect';
+import MuiRadio from './components/MUI/MuiRadio';
 
 function App() {
   return (
     <div className="App">
-      {/* <ClickCounter name={'react developer '}/>
+      <ClickCounter name={'react developer '}/>
       <HoverCounter/>
       <MuiTypography/>
       <MuiButton/>
-      <MuiTextFields/> */}
+      <MuiTextFields/>
       <MuiSelect/>
+      <MuiRadio/>
     </div>
   );
 }
--- src/components/MUI/MuiSelect.jsx
+++ src/components/MUI/MuiSelect.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
-import { Box } from '@mui/material'
+import React, {useState} from 'react'
+import { Box,TextField,MenuItem, Stack} from '@mui/material'
+
+// const nameList = ["Bhanu", "Prakash", "Akash", "React", "Angular","Python"]
 
 const MuiSelect = () => {
+    const [num, setNum] = useState("")
+    // const[names, setNames] = useState([])
+    // console.log(names)
+
+    // console.log({country})
+
+    const handleNum = (event) =>{
+        setNum(event.target.value)
+    }
+
+    // const handleNames = (event) =>{
+    //    const value = event.target.value 
+    //    setNames(typeof value === 'string' ? value.split(","):value)
+    // }
+
   return (
-    <Box width='250px' border='2px solid red'>
-        Miui select
-    </Box>
+    <Stack direction="row"   spacing={5} flexWrap='wrap' margin="10px">
+        <Stack   spacing={5}>
+            <Box width='250px'>
+            <TextField label="Select Num" select value={num} fullWidth onChange={handleNum} helperText="with label">
+                    <MenuItem value="1">ONE</MenuItem>
+                    <MenuItem value="2">TWO</MenuItem>
+                    <MenuItem value="3">THREE</MenuItem>
+            </TextField>
+            </Box>
+        </Stack>
+        <Stack   spacing={5}>
+        <Box width='250px'>
+            <TextField select value={num} fullWidth onChange={handleNum} helperText="without label" InputProps={{ 'aria-label': 'Without label'}}>
+                    <MenuItem value=""><em>None</em></MenuItem>
+                    <MenuItem value="1">ONE</MenuItem>
+                    <MenuItem value="2">TWO</MenuItem>
+                    <MenuItem value="3">THREE</MenuItem>
+            </TextField>
+        </Box>
+        </Stack>
+        <Stack   spacing={5}>
+        <Box width='250px'>
+            <TextField select value={num} fullWidth onChange={handleNum} helperText="error-field" InputProps={{ 'aria-label': 'Without label'}} error={true}>
+                    <MenuItem value=""><em>None</em></MenuItem>
+                    <MenuItem value="1">ONE</MenuItem>
+                    <MenuItem value="2">TWO</MenuItem>
+                    <MenuItem value="3">THREE</MenuItem>
+            </TextField>
+        </Box>
+        </Stack>
+        <Stack   spacing={5}>
+        <Box width='250px'>
+            <TextField select value={num} fullWidth onChange={handleNum} helperText="disabled" InputProps={{ 'aria-label': 'Without label'}} disabled>
+                    <MenuItem value=""><em>None</em></MenuItem>
+                    <MenuItem value="1">ONE</MenuItem>
+                    <MenuItem value="2">TWO</MenuItem>
+                    <MenuItem value="3">THREE</MenuItem>
+            </TextField>
+        </Box>
+        </Stack>
+        <Stack   spacing={5}>
+        <Box width='250px'>
+            <TextField select value={num} fullWidth onChange={handleNum} helperText="read-only" InputProps={{readOnly:true}}>
+                    <MenuItem value=""><em>None</em></MenuItem>
+                    <MenuItem value="1">ONE</MenuItem>
+                    <MenuItem value="2">TWO</MenuItem>
+                    <MenuItem value="3">THREE</MenuItem>
+            </TextField>
+        </Box>
+        </Stack>
+    </Stack> 
   )
 }
 
